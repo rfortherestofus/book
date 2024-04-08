f961fdef0f563c65c5052f22f41b364631a71a68
David Keyes <david@rfortherestofus.com>
2024-04-08 11:44:36 -0700

    Update crash course chapter

---
+++ _common.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  dpi = 150,
+  out.width = "100%",
+  dev = "ragg_png"
+)
+
+options(
+  scipen = 999,
+  pillar.sigfig = 5,
+  width = 75
+)
